ca83eaadc99544c606cbf9fb2cba95ccb8c034de
Hakimsalah <hakim.salah@ensi-uma.tn>
2025-04-16 13:47:04 +0100

    hakim back

--- backend/src/main/java/com/pcd/backend/BackendApplication.java
+++ backend/src/main/java/com/pcd/backend/BackendApplication.java
@@ -1,7 +1,12 @@
 package com.pcd.backend;
 
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class BackendApplication {
@@ -10,4 +15,12 @@ public class BackendApplication {
 		SpringApplication.run(BackendApplication.class, args);
 	}
 
+	@Bean
+    CommandLineRunner init() {
+        return args -> {
+            // Crée le dossier uploads/ s'il n'existe pas
+            Files.createDirectories(Paths.get("uploads/"));
+            System.out.println("Dossier uploads/ créé ou déjà existant");
+        };
+    }
 }
