96e7569941e7c153895fe7a3e23e25e604e4a582
Hakimsalah <hakim.salah@ensi-uma.tn>
2025-04-30 22:52:41 +0100

    corercted Spring Secuity Config file

--- backend/src/main/java/com/pcd/backend/config/SecurityConfig.java
+++ backend/src/main/java/com/pcd/backend/config/SecurityConfig.java
@@ -1,6 +1,6 @@
 package com.pcd.backend.config;
 
-<<<<<<< HEAD
+
 import com.pcd.backend.user.JwtUtil;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
